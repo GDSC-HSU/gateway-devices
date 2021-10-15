2722176e4bc0b9fb1580c2a69dbb1c2ad206eae5
truonghoangduy <truonghoangduy03111999@gmail.com>
2021-10-15 17:18:16 +0700

    dart BLE UUID genration

--- ble_uuid_generator.py
+++ ble_uuid_generator.py
@@ -3,6 +3,8 @@ import yaml
 
 pathTOHeaderFile = "./ESP32-BLE/src/BLE_DESIGN.g.h"
 
+pathToDartFile = "../lib/config/constants/ble_desgin_constants.g.dart"
+
 
 def read_config():
     with open("./ble_uuid_conf.yml") as f:
@@ -12,25 +14,45 @@ def read_config():
         return(data["gatt_profile"])
 
 
-def cHeaderFileLimbo(data):
+def cHeaderFileLimbo(listOfData):
     output = ""
-    for key, value in data.items():
-        text = '''#ifndef {key}\n#define {key} "{value}"\n#endif
-        '''.format(key=key, value=value)
-        output += text + "\n"
+    for i in listOfData:
+        for key, value in i.items():
+            text = '''#ifndef {key}\n#define {key} "{value}"\n#endif
+            '''.format(key=key, value=value)
+            output += text + "\n"
     return output
 
 
-def dartCode():
-    print("Not impl yet")
+def dartCode(listOfData):
+    # import required library
+    output = "import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';\n"
+    servicesAsList = ""
+    geneteuuid = ""
+    for i in listOfData:
+        for key, value in i.items():
+            text = '''Uuid {key} = Uuid.parse("{value}");
+            '''.format(key=key, value=value)
+            geneteuuid += text + "\n"
+            if "SERVICE" in key:
+                servicesAsList += '''{key},'''.format(key=key)
+
+    servicesUUIDasListExport = '''List<Uuid> deviceServices = [{services}];'''.format(
+        services=servicesAsList)
+    return output+geneteuuid+"\n"+servicesUUIDasListExport
 
 
-def generate():
+def generate(fileGenrateCallback, fileLocation):
     text = "//THIS IS GENERATED IN ble_uuid_conf.yml\n"
-    for i in read_config():
-        text += cHeaderFileLimbo(i)
-    f = open(pathTOHeaderFile, 'w')
+    text += fileGenrateCallback(read_config())
+    f = open(fileLocation, 'w')
     f.write(text)
+    f.close()
 
 
-generate()
+def run():
+    generate(dartCode, pathToDartFile)
+    generate(cHeaderFileLimbo, pathTOHeaderFile)
+
+
+run()
