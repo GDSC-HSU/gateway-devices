2e0b64142e6a938e4c1fd21b598d7451bd274e3d
truonghoangduy <truonghoangduy03111999@gmail.com>
2022-03-14 23:37:46 +0700

    feat: :rocket: implement ours libary sensor

--- ESP32-BLE/src/MySensor.cpp
+++ ESP32-BLE/src/MySensor.cpp
@@ -1,34 +1,75 @@
 #include "MySensor.h"
 
+MySensor::MySensor() {}
 
-void MySensor::isSensorBeginFail(bool beginResult, String sensorName){
+void MySensor::isSensorBeginFail(bool beginResult, String sensorName)
+{
     if (!beginResult)
     {
         Serial.println("[Dev Error] sensor fail " + sensorName);
     }
 }
 
-void MySensor::init(MyBLE *ble){
-    pMyBLE = ble;
-    initThermometer();
-    initNFC();
+void MySensor::log(String text)
+{
+    Serial.println(text);
+}
+
+void MySensor::init(MyBLE *myBLE, NfcAdapter *nfc, Adafruit_MLX90614 *mlx)
+{
+    ptr_MyBLE = myBLE;
+    initThermometer(mlx);
+    initNFC(nfc);
     initRadarProximity();
 }
-void MySensor::initThermometer(){
-    bool isWireBegin = Wire.begin(SDA_PIN,SDL_PIN);
-    isSensorBeginFail(isWireBegin,"Wire: Adafruit_MLX90614");
-    mlx = Adafruit_MLX90614();
-    bool isMlxBegin = mlx.begin();
-    isSensorBeginFail(isMlxBegin,"Adafruit_MLX90614");
+void MySensor::initThermometer(Adafruit_MLX90614 *mlx)
+{
+    bool isWireBegin = Wire.begin(SDA_PIN, SDL_PIN);
+    isSensorBeginFail(isWireBegin, "Wire: Adafruit_MLX90614");
+    ptr_MLX = mlx;
+    bool isMlxBegin = ptr_MLX->begin();
+    isSensorBeginFail(isMlxBegin, "Adafruit_MLX90614");
 }
 
-void MySensor::initRadarProximity(){
-    pinMode(RADAR_PIN,INPUT);
+void MySensor::initRadarProximity()
+{
+    pinMode(RADAR_PIN, INPUT);
 }
 
-void MySensor::initNFC(){
-    // spi_interface = PN532_SPI(SPI,SPI_CS);
-    // nfc = NfcAdapter(spi_interface);
-    // nfc.begin();
-    // NO LOG
+void MySensor::initNFC(NfcAdapter *nfc)
+{
+    ptr_NFC = nfc;
+    ptr_NFC->begin();
+}
+
+void MySensor::readRFID()
+{
+    // void readNFC()
+    // {
+    if (ptr_NFC->tagPresent())
+    {
+        NfcTag tag = ptr_NFC->read();
+        String tagID = tag.getUidString();
+        ptr_MyBLE->setRFID(tagID);
+    }
+    // }
+}
+
+bool MySensor::isMotionApper()
+{
+    // TODO add
+    int isMotionApper = digitalRead(RADAR_PIN);
+    String isMotionApperAsString = String(ptr_MLX->readObjectTempC());
+    ptr_MyBLE->setRadar(isMotionApperAsString);
+    if (isMotionApper == HIGH)
+    {
+        return true;
+    }
+    return false;
+}
+
+void MySensor::readThermometer()
+{
+    String temp = String(ptr_MLX->readObjectTempC());
+    ptr_MyBLE->setThermometer(temp);
 }
--- ESP32-BLE/src/MySensor.h
+++ ESP32-BLE/src/MySensor.h
@@ -2,48 +2,52 @@
 #include "sensor_pin.h"
 
 /// dependency libary
+#ifndef Adafruit_MLX90614_H
+#define Adafruit_MLX90614_H
 #include "Adafruit_MLX90614.h"
-#include <PN532_SPI.h>
-#include <PN532.h>
-#include <NfcAdapter.h>
+#endif
+///
+
+#include "MyBLE.h"
+
+#include "NfcAdapter.h"
 ///
 #ifndef MYSENSOR_H_
 #define MYSENSOR_H_
 class MySensor
 {
 private:
+    // (SPI, 5);
 
-    PN532_SPI spi_interface;
-    // (SPI, 5); 
+    MyBLE *ptr_MyBLE;
 
     /* data */
-    Adafruit_MLX90614 mlx;
-    NfcAdapter nfc;
+    Adafruit_MLX90614 *ptr_MLX;
+    NfcAdapter *ptr_NFC;
     //
-
-    MyBLE *pMyBLE;
-
-    void initNFC();
     void initProximity();
 
     // init Adafruit_MLX90614
-    void initThermometer();
+    void initThermometer(Adafruit_MLX90614 *mlx);
 
-    // int 
+    // int
     void initRadarProximity();
+    //
+    void initNFC(NfcAdapter *nfc);
+    void log(String text);
+
     //
 
     void isSensorBeginFail(bool beginResult, String sensorName);
 
 public:
     MySensor();
-    ~MySensor();
 
-    void init(MyBLE *myBLE);
+    void init(MyBLE *myBLE, NfcAdapter *nfc, Adafruit_MLX90614 *mlx);
     bool isMotionApper();
-    String readRFID();
-    String readProximity();
-    String readThermometer();
+    void readRFID();
+    void readProximity();
+    void readThermometer();
 
     void notifyRFID(String value);
     void notifyProximity(String value);
